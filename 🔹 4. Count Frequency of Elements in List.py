d1fc9eb410578614b9c0206cc2eed514458775ec
mounika <189mounika.b@gmail.com>
2026-04-14 00:00:16 +0530

    new

--- 🔹 4. Count Frequency of Elements in List.py
+++ 🔹 4. Count Frequency of Elements in List.py
@@ -5,10 +5,3 @@ for i in lst:
     freq[i] = freq.get(i, 0) + 1
 
 print(freq)
-
-
-sentence = "Python is very powerful programming language"
-words = sentence.split()
-
-largest = max(words, key=len)
-print("Largest word:", largest)
