5d5ce1d7669d9826b3dc3a180ddbd750f4a3d501
mounika <189mounika.b@gmail.com>
2026-04-13 15:20:12 +0530

    most and least frequent nuimber

---
+++ mostfrequent.py
@@ -0,0 +1,27 @@
+numbers = [1, 2, 2, 3, 3, 3, 4, 5]
+
+freq = {}
+
+for num in numbers:
+    if num in freq:
+        freq[num] += 1
+    else:
+        freq[num] = 1
+
+max_count = 0
+min_count = len(numbers)
+most_frequent = None
+least_frequent = None
+
+
+for key in freq:
+    if freq[key] > max_count:
+        max_count = freq[key]
+        most_frequent = key
+
+    if freq[key] < min_count:
+        min_count = freq[key]
+        least_frequent = key
+
+print("Most frequent:", most_frequent)
+print("Least frequent:", least_frequent)
