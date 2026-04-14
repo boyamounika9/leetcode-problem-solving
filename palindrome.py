97c0cce3004475f9c44c200f7ef0ba86d829e73f
mounika <189mounika.b@gmail.com>
2026-04-14 14:48:31 +0530

    palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,13 @@
 x=int(input())
-y=str(x)
-reverse=y[::-1]
-if reverse==y:
+temp=x
+reverse=0
+if x<0:
+    print("false")
+while x!=0:
+    digit=x%10
+    reverse=reverse*10+digit
+    x//=10
+if temp==reverse:
     print("true")
 else:
     print("false")
