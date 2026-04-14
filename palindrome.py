2a398fad15756600793e857285040953093b4ed0
mounika <189mounika.b@gmail.com>
2026-04-14 14:52:18 +0530

    palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,20 @@
-x=int(input())
-temp=x
-reverse=0
-if x<0:
-    print("false")
-while x!=0:
-    digit=x%10
-    reverse=reverse*10+digit
-    x//=10
-if temp==reverse:
-    print("true")
-else:
-    print("false")
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # t=bool("true")
+        # f=bool("false")
+        temp=x
+        reverse=0
+        if x<0:
+            return False
+        while x!=0:
+            digit=x%10
+            reverse=reverse*10+digit
+            x//=10
+        if temp==reverse:
+            return True 
+        else:
+            return False
+s=Solution()
+s.isPalindrome(121)
+s.isPalindrome(-121)
+s.isPalindrome(10)
