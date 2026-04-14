ac93f100d12ec8c074a5ee801cffefa99d376834
mounika <189mounika.b@gmail.com>
2026-04-14 15:07:48 +0530

    counter method

--- mostfrequent.py
+++ mostfrequent.py
@@ -1,27 +1,32 @@
-numbers = [1, 2, 2, 3, 3, 3, 4, 5]
+# numbers = [1, 2, 2, 3, 3, 3, 4, 5]
 
-freq = {}
+# freq = {}
 
-for num in numbers:
-    if num in freq:
-        freq[num] += 1
-    else:
-        freq[num] = 1
+# for num in numbers:
+#     if num in freq:
+#         freq[num] += 1
+#     else:
+#         freq[num] = 1
 
-max_count = 0
-min_count = len(numbers)
-most_frequent = None
-least_frequent = None
+# max_count = 0
+# min_count = len(numbers)
+# most_frequent = None
+# least_frequent = None
 
 
-for key in freq:
-    if freq[key] > max_count:
-        max_count = freq[key]
-        most_frequent = key
+# for key in freq:
+#     if freq[key] > max_count:
+#         max_count = freq[key]
+#         most_frequent = key
 
-    if freq[key] < min_count:
-        min_count = freq[key]
-        least_frequent = key
+#     if freq[key] < min_count:
+#         min_count = freq[key]
+#         least_frequent = key
 
-print("Most frequent:", most_frequent)
-print("Least frequent:", least_frequent)
+# print("Most frequent:", most_frequent)
+# print("Least frequent:", least_frequent)
+
+from collections import Counter
+a="boya mounika"
+b=Counter(a)
+print(b)
